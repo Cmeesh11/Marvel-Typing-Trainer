f9b0832519b796a3875d60a2e8a2db05b81019b1
Cmeesh11 <cartermeesh11@gmail.com>
2023-02-13 18:30:55 -0500

    removed serverside parameters for api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,8 @@
-// Defining API keys and creating a timestamp
+
 var publicKey = "c80a5387467017b31f13477fc4481d74";
-var privateKey = "747b04935d0b762d907eae78288967013a942b90";
-var timestamp = (new Date()).toString();
-// Using CryptoJS library to encrypt hash into MD5 digest
-var hash = CryptoJS.MD5(timestamp + privateKey + publicKey);
+
 // Fetching Marvel API
-fetch("https://gateway.marvel.com/v1/public/stories?ts=" + timestamp + "&apikey=" + publicKey + "&hash=" + hash)
+fetch("https://gateway.marvel.com/v1/public/stories?apikey=" + publicKey)
 .then((response) => {
   return response.json();
 })
